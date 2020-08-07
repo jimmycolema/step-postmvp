432376496fafa5bebe0dbce35c58ef18fa4b0d7d
Jimmy <jimmycolema@google.com>
2020-08-07 14:42:58 +0000

    Make filenames constants, add if name==main guard

--- utils/services/goodreads_dataset_parser.py
+++ utils/services/goodreads_dataset_parser.py
@@ -1,20 +1,24 @@
 import csv
 import json
 
+# Constants for corresponding indexes 
 ENTRY_INDEX = 0
 TITLE_INDEX = 1
 AUTHOR_INDEX = 2
 ISBN_INDEX = 4
 
+DATASET_PWD = "../data/books.csv"
+OUTPUT_FILE_PWD = "../data/parsed_books.txt"
+
 # Iterates over the Goodreads-books dataset, converting each entry
 # into a book dictionary compatible with the existing CoffeeHouse
 # architecture.
-def parse_goodreads_dataset():
-  with open("../data/books.csv") as goodreads_dataset:
-    with open("../data/parsed_books.txt", "w") as books_file:
+def parse_goodreads_dataset(dataset_pwd, output_file_pwd):
+  with open(DATASET_PWD) as goodreads_dataset:
+    with open(OUTPUT_FILE_PWD, "w") as books_file:
       goodreads_csv = csv.reader(goodreads_dataset)
 
-      next(goodreads_csv)
+      next(goodreads_csv) # Skip CSV file column headers.
       for row in goodreads_csv:
         title = row[TITLE_INDEX]
         author = row[AUTHOR_INDEX]
@@ -28,4 +32,5 @@ def parse_goodreads_dataset():
         book_json = json.dumps(book)
         books_file.writelines((book_json, '\n'))
 
-parse_goodreads_dataset()
+if __name__ == "__main__":
+  parse_goodreads_dataset(DATASET_PWD, OUTPUT_FILE_PWD)
